22d6e53964b6a4f5100d36b997e3896097b0fe7b
Ayush Soni <soni.ayush.2212@gmail.com>
2022-08-20 00:12:01 +0530

    add color changing functionality

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const gridContainer = document.querySelector(".grid-container");
 
+// Creates a grid
 for (let i = 0; i < 256; i++) {
   const gridBox = document.createElement("div");
   gridBox.classList.add("grid-box");
   gridContainer.append(gridBox);
 }
+
+const gridSquares = document.querySelectorAll(".grid-box");
+
+gridSquares.forEach((square) => {
+  square.addEventListener("mouseover", () => {
+    square.classList.add("grey");
+  });
+});
